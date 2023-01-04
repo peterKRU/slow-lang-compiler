e3bb2c4500f9318b539142eedc8ebe85481fe591
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-04 13:17:13 +0200

    refactor: remove unused components from bytecode_generator
    Remove ParseTreeWalker from bytecode_generator. Reworked version of the class moved to compiler package.
    Remove InfixToPostfixConverter from bytecode_generator. Class replaced by ShuntingYard in compiler package.
    Refactor Expression translator class.
    Refactor BytecodeGenerator class.

--- src/main/java/bytecode_generator/BytecodeGenerator.java
+++ src/main/java/bytecode_generator/BytecodeGenerator.java
@@ -1,20 +1,13 @@
 package bytecode_generator;
 
-import java.util.List;
-
 import org.antlr.v4.runtime.tree.ParseTree;
 
 public class BytecodeGenerator {
 
 	public static int[] generateBytecode(ParseTree parseTree) {
 		
-		List<String> tokensList = ParseTreeWalker.convertToList(parseTree);
-		List<Integer> bytecodesList = ExpressionTranslator.translateExpressions(tokensList);
-
-		bytecodesList.add(Bytecodes.HALT);
-		int[] bytecodesArray = bytecodesList.stream().mapToInt(i -> i).toArray();		
 		
-		return bytecodesArray;
+		return null;
 	}	
 	
 }
--- src/main/java/bytecode_generator/ExpressionTranslator.java
+++ src/main/java/bytecode_generator/ExpressionTranslator.java
@@ -1,96 +1,11 @@
 package bytecode_generator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ExpressionTranslator {
 
-	public static List<Integer> translateExpressions(List<String> parseTreeList) {
-
-		List<Integer> bytecodeList = new ArrayList<Integer>();
-
-		for (String token : parseTreeList) {
-
-			if (Vocabulary.isOperator(token)) {
-
-				switch (token) {
-
-				case "=" -> {
-					bytecodeList.add(Bytecodes.ASS);
-				}
-				case "+" -> {
-					bytecodeList.add(Bytecodes.IADD);
-				}
-				case "-" -> {
-					bytecodeList.add(Bytecodes.ISUB);
-				}
-				case "*" -> {
-					bytecodeList.add(Bytecodes.IMUL);
-				}
-				case "/" -> {
-					bytecodeList.add(Bytecodes.IDIV);
-				}
-				case "||" -> {
-					bytecodeList.add(Bytecodes.OR);
-				}
-				case "&&" -> {
-					bytecodeList.add(Bytecodes.AND);
-				}
-				case "==" -> {
-					bytecodeList.add(Bytecodes.IEQ);
-				}
-				case "!=" -> {
-					bytecodeList.add(Bytecodes.IEQ);
-					bytecodeList.add(Bytecodes.NOT);
-				}
-				case ">" -> {
-					bytecodeList.add(Bytecodes.IGT);
-				}
-				case "<" -> {
-					bytecodeList.add(Bytecodes.ILT);
-				}
-				default -> {
-					bytecodeList.add(777777);
-				}
-
-				}
-			}
-
-			else if (isIntegerType(token)) {
-
-				bytecodeList.add(Bytecodes.LSTORE);
-				bytecodeList.add(Integer.valueOf(token.substring(1)));
-			}
-
-			else if (isIdentifierType(token)) {
-
-				bytecodeList.add(Bytecodes.LREF);
-				bytecodeList.add(token.substring(1).hashCode());
-			}
-
-			else if (isEndOfFile(token)) {
-
-				bytecodeList.add(Bytecodes.HALT);
-			}
-
-		}
-
-		return bytecodeList;
+	public static List<Integer> translateExpressions(){
+		
+		return null;
 	}
-
-	public static boolean isIntegerType(String token) {
-
-		return token.charAt(0) == '$';
-	}
-
-	public static boolean isIdentifierType(String token) {
-
-		return token.charAt(0) == '#';
-	}
-
-	public static boolean isEndOfFile(String token) {
-
-		return token == "EOF";
-	}
-
 }
