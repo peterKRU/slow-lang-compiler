9e26a8d4f28b1c79ae0d9b3c782032fe5cb0219a
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-04 12:32:34 +0200

    feat: add ParseTreeWalker
    Introduce ParseTreeWalker class which walks an antlr4 ParseTree object in order, creates a ParsedToken object out of each terminal node and puts it into a list of ParsedToken objects.

---
+++ src/main/java/compiler/ParseTreeWalker.java
@@ -0,0 +1,38 @@
+package compiler;
+
+import java.util.List;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import bytecode_generator.Vocabulary;
+
+public class ParseTreeWalker {
+
+	public static void walkTree(ParseTree parseTree, List<ParsedToken> outputTokens) {
+
+		if (parseTree instanceof TerminalNode) {
+
+			String nodeValue = parseTree.getText();
+			int nodeType = ((TerminalNode) parseTree).getSymbol().getType();
+
+			if (!isEndOfFile(nodeType)) {
+
+				String typeName = Vocabulary.TOKEN_NAMES[nodeType];
+				outputTokens.add(new ParsedToken(typeName, nodeValue));
+			}
+
+		} else {
+
+			for (int i = 0; i < parseTree.getChildCount(); i++) {
+				walkTree(parseTree.getChild(i), outputTokens);
+			}
+		}
+
+	}
+
+	private static boolean isEndOfFile(int tokenTypeIndex) {
+
+		return tokenTypeIndex == -1;
+	}
+
+}
--- src/main/java/compiler/ParsedToken.java
+++ src/main/java/compiler/ParsedToken.java
@@ -2,4 +2,31 @@ package compiler;
 
 public class ParsedToken {
 
+	private String type;
+	private String value;
+
+	public ParsedToken(String type, String value) {
+
+		this.type = type;
+		this.value = value;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void addModifier(String modifier) {
+
+		value = modifier + value;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + type + " : " + value + "]";
+	}
+
 }
