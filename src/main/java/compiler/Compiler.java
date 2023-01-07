92e14272f040f642d7b35fe4641335053d843f36
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-07 18:02:47 +0200

    fix: add @SuppressWarnings(deprecation) in Compiler class

--- src/main/java/compiler/Compiler.java
+++ src/main/java/compiler/Compiler.java
@@ -6,19 +6,19 @@ import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+@SuppressWarnings("deprecation")
 public class Compiler {
 
 	private Lexer lexer;
 	private Parser parser;
 
-	@SuppressWarnings("deprecation")
 	public ParseTree compile(String fileName) {
 
 		String rawTokens = FileReader.readFile(fileName);
 		lexer = new SlowLangV1Lexer(new ANTLRInputStream(rawTokens));
 		parser = new SlowLangV1Parser(new CommonTokenStream(lexer));
 		ParseTree parseTree = ((SlowLangV1Parser) parser).program();
-
+		
 		return parseTree;
 	}
 	
