bf11e917405c2a127f8f2e7d1b8687ecd56552d9
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-28 17:03:21 +0200

    feat: implement hashCode() and equals() methods of ParsedToken

--- src/main/java/compiler/ParsedToken.java
+++ src/main/java/compiler/ParsedToken.java
@@ -1,5 +1,7 @@
 package compiler;
 
+import java.util.Objects;
+
 public class ParsedToken {
 
 	private String type;
@@ -27,6 +29,23 @@ public class ParsedToken {
 	@Override
 	public String toString() {
 		return "[" + type + " : " + value + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(type, value);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ParsedToken other = (ParsedToken) obj;
+		return Objects.equals(type, other.type) && Objects.equals(value, other.value);
 	}
 
 }
