e4d512bc1762c7b2097fbf96f40d2c59371ffbdc
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-28 16:55:12 +0200

    fix: fix bug in Runner

--- src/main/java/compiler/Runner.java
+++ src/main/java/compiler/Runner.java
@@ -2,10 +2,7 @@ package compiler;
 
 import java.io.IOException;
 
-import org.antlr.v4.runtime.tree.ParseTree;
-
 import bytecode_generator.BytecodeExporter;
-import bytecode_generator.BytecodeGenerator;
 
 public class Runner {
 
@@ -14,9 +11,8 @@ public class Runner {
 
 		String fileName = "test_source.txt";
 		Compiler compiler = new Compiler();
-		ParseTree parseTree = compiler.compile(fileName);
 
-		int[] bytecode = BytecodeGenerator.generateBytecode(parseTree);
+		int[] bytecode = compiler.compile(fileName);
 
 		try {
 			BytecodeExporter.export(bytecode, "test_compiled.txt");
