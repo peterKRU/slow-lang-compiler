c68af419bb24fffb72a2a27355e97f3e1c39aa91
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-30 15:11:29 +0200

    refactor: make Exporter and TokenTranslator required interfaces in BytecodeGenerator

--- src/main/java/bytecode_generator/BytecodeGenerator.java
+++ src/main/java/bytecode_generator/BytecodeGenerator.java
@@ -1,21 +1,37 @@
 package bytecode_generator;
 
+import java.io.IOException;
 import java.util.List;
 
 import compiler.ParsedToken;
 
 public class BytecodeGenerator implements BytecodeCompiler {
-	
-	private static ExpressionTranslator expressionTranslator;
-	
-	public static int[] generateBytecode(List<ParsedToken> parsedTokens) {
-		
-		expressionTranslator = new ExpressionTranslator();
-		
-		List<Integer> translatedExpressions = expressionTranslator.translateExpressions(parsedTokens);
+
+	private TokenTranslator tokenTranslator;
+	private Exporter exporter;
+
+	public BytecodeGenerator() {
+
+		this.tokenTranslator = new ExpressionTranslator();
+		this.exporter = new BytecodeExporter();
+	}
+
+	public int[] generateBytecode(List<ParsedToken> parsedTokens) {
+
+		List<Integer> translatedExpressions = tokenTranslator.translateTokens(parsedTokens);
 		int[] bytecodesArray = translatedExpressions.stream().mapToInt(i -> i).toArray();
 
 		return bytecodesArray;
+	}
+
+	public void exportBytecode(int[] bytecode, String filePath) {
+
+		try {
+			exporter.export(bytecode, filePath);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
 	}
 
 	@Override
