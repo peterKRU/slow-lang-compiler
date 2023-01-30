2011621ed0d3fc6d0a696957df24313874f40047
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-30 15:02:15 +0200

    feat: add Exporter interface to bytecodes package

--- src/main/java/bytecode_generator/Exporter.java
+++ src/main/java/bytecode_generator/Exporter.java
@@ -1,5 +1,8 @@
 package bytecode_generator;
 
-public interface Exporter {
+import java.io.IOException;
 
+public interface Exporter {
+	
+	public void export(int[] content, String filePath) throws IOException;
 }
