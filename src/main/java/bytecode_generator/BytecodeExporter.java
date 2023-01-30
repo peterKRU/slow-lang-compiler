a7640e6eef44517eae4edcb5079701c198d844b4
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-30 15:04:39 +0200

    feat: add Exporter as provided interface in BytecodeExporter

--- src/main/java/bytecode_generator/BytecodeExporter.java
+++ src/main/java/bytecode_generator/BytecodeExporter.java
@@ -6,13 +6,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
-public class BytecodeExporter {
+public class BytecodeExporter implements Exporter{
 
-	public static void export(int[] array, String filePath) throws IOException {
+	@Override
+	public void export(int[] content, String filePath) throws IOException {
 
 		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath))) {
 
-			Arrays.stream(array).mapToObj(Integer::toString).forEach(s -> {
+			Arrays.stream(content).mapToObj(Integer::toString).forEach(s -> {
 
 				try {
 					writer.write(s);
