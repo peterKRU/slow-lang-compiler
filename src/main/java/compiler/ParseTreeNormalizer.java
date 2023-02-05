ddd03a428faa6f15216ba4b70cc6cbd6e12e70fe
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-05 16:13:35 +0200

    feat: implement ParseTreeNormalizer

---
+++ src/main/java/compiler/ParseTreeNormalizer.java
@@ -0,0 +1,172 @@
+package compiler;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ParseTreeNormalizer {
+
+	public static List<ParsedToken> normalize(List<ParsedToken> parsedTokens) {
+
+		List<List<ParsedToken>> blocks = splitBlocks(parsedTokens);
+		List<List<ParsedToken>> normalizedBlocks = new ArrayList<List<ParsedToken>>();
+
+		for (List<ParsedToken> block : blocks) {
+
+			ParsedToken identifierToken = block.get(0);
+
+			if (isMainBlock(identifierToken)) {
+
+				List<ParsedToken> normalizedMainExecutionBlock = normalizeMainExecutionBlock(block);
+				normalizedBlocks.add(normalizedMainExecutionBlock);
+
+			} else if (isClass(identifierToken)) {
+
+				List<ParsedToken> normalizedClass = normalizeClass(block);
+				normalizedBlocks.add(normalizedClass);
+			}
+		}
+		
+		System.out.println(normalizedBlocks);
+		
+		return null;
+	}
+
+	private static List<List<ParsedToken>> splitBlocks(List<ParsedToken> parsedTokens) {
+
+		List<List<ParsedToken>> blocks = new ArrayList<List<ParsedToken>>();
+		List<ParsedToken> currentBlock = new ArrayList<ParsedToken>();
+
+		for (int i = 0; i < parsedTokens.size(); i++) {
+
+			ParsedToken token = parsedTokens.get(i);
+
+			if (token.getType() == "CLASS") {
+
+				blocks.add(currentBlock);
+				currentBlock = new ArrayList<ParsedToken>();
+			}
+
+			currentBlock.add(token);
+		}
+
+		blocks.add(currentBlock);
+
+		return blocks;
+	}
+
+	private static List<ParsedToken> normalizeMainExecutionBlock(List<ParsedToken> mainExecutionBlockTokens) {
+
+		return null;
+	}
+
+	private static List<ParsedToken> normalizeClass(List<ParsedToken> classTokens) {
+
+		ParsedToken classId = classTokens.get(1);
+		List<List<ParsedToken>> methods = new ArrayList<List<ParsedToken>>();
+		List<ParsedToken> currentMethod = new ArrayList<ParsedToken>();
+
+		System.out.println("TOKENS:::::::" + classTokens);
+
+		for (int i = 2; i < classTokens.size(); i++) {
+
+			ParsedToken currentToken = classTokens.get(i);
+
+			if (isTypeDeclaration(currentToken)) {
+
+				ParsedToken nextToken = classTokens.get(i + 2);
+
+				if (isMethodDeclaration(nextToken)) {
+
+					List<ParsedToken> methodParameters = new ArrayList<ParsedToken>();
+					ParsedToken methodId = classTokens.get(i + 1);
+
+					currentMethod.add(methodId);
+
+					for (int parametersBlockStart = i + 3; parametersBlockStart < classTokens
+							.size(); parametersBlockStart++) {
+
+						ParsedToken parameterToken = classTokens.get(parametersBlockStart);
+
+						if (parameterToken.getType() == "RPAREN") {
+
+							i = parametersBlockStart + 2;
+							break;
+						}
+
+						if (parameterToken.getType() == "ID") {
+
+							methodParameters.add(parameterToken);
+						}
+					}
+
+					List<ParsedToken> methodBody = new ArrayList<ParsedToken>();
+
+					for (int methodBodyStart = i; methodBodyStart < classTokens.size(); methodBodyStart++) {
+
+						ParsedToken methodBodyToken = classTokens.get(methodBodyStart);
+
+						if (methodBodyToken.getType() == "RBRACE") {
+
+							i = methodBodyStart;
+							break;
+						}
+
+						methodBody.add(methodBodyToken);
+					}
+
+					Integer parametersCount = methodParameters.size();
+					ParsedToken parametersCountToken = new ParsedToken("INT", parametersCount.toString());
+					currentMethod.add(parametersCountToken);
+					currentMethod.addAll(methodParameters);
+
+					// List<List<ParsedToken>> splitMethodBody = splitExpressions(methodBody);
+
+					System.out.println("OLOLO " + methodBody);
+
+					currentMethod.addAll(methodBody);
+
+					methods.add(currentMethod);
+					currentMethod = new ArrayList<ParsedToken>();
+				}
+			}
+
+		}
+
+		for (List<ParsedToken> method : methods) {
+
+			System.out.println("METHOD" + method);
+		}
+
+		return null;
+	}
+
+	private static boolean isTypeDeclaration(ParsedToken token) {
+
+		return token.getType() == "INT_TYPE";
+	}
+
+	private static boolean isMethodDeclaration(ParsedToken nextToken) {
+
+		return nextToken.getType() == "LPAREN";
+	}
+
+	private static boolean isVariableDeclaration(ParsedToken nextToken) {
+
+		return nextToken.getType() != "LPAREN";
+	}
+
+	private static boolean isMainBlock(ParsedToken identifierToken) {
+
+		return identifierToken.getType() == "MAIN";
+	}
+
+	private static boolean isClass(ParsedToken identifierToken) {
+
+		return identifierToken.getType() == "CLASS";
+	}
+}
