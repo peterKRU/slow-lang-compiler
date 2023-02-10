bc5a6211ac9f9ed3e0a9442f018fedea3cdf6bc2
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-10 16:06:50 +0200

    feat: implement console which takes input from the user

--- src/main/java/compiler/Runner.java
+++ src/main/java/compiler/Runner.java
@@ -1,15 +1,48 @@
 package compiler;
 
+import java.util.Scanner;
+
 public class Runner {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
 
-		String fileName = "test_classes_source.txt";
-		Compiler compiler = new Compiler();
+		while (true) {
 
-		byte[] bytecode = compiler.compile(fileName);
+			log(ConsoleUtils.DEFAULT_STATIC_MESSAGE);
+
+			String input = scanner.nextLine();
+
+			if (input.equals(ConsoleUtils.EXIT)) {
+
+				break;
+			}
+
+			switch (input) {
+
+				case ConsoleUtils.TEST -> {
+					
+					Compiler compiler = new Compiler();
+					byte[] bytecode = compiler.compile(ConsoleUtils.DEFAULT_TEST_FILE);
+				}
+	
+				default -> {
+	
+					Compiler compiler = new Compiler();
+					byte[] bytecode = compiler.compile(input);
+				}
+
+			}
+
+		}
+
+		scanner.close();
 	}
+	
+	private static void log(String message) {
 
+		System.out.println(message);
+	}	
 }
